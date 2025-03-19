fa285dfece2ec0244071b9c11cffe503e3e05ad2
kauatorresfranca <kauatorresfranca2@gmail.com>
2025-03-19 14:16:27 -0300

    adding tests

--- bookStore/urls.py
+++ bookStore/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, re_path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    re_path('bookstore/(?P<version>(v1|v2))/', include('order.urls')),
-    re_path('bookstore/(?P<version>(v1|v2))/', include('order.urls'))
+    re_path(r'bookstore/(?P<version>(v1|v2))/', include('order.urls')),
+    re_path(r'bookstore/(?P<version>(v1|v2))/', include('product.urls')),
 ]
+
